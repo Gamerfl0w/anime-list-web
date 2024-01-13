805497b6c2e964066d26d4ec05e30566c31887d4
gamerfl0w <jluisdechavez@gmail.com>
2024-01-13 21:18:32 +0800

    idk how, but graphql api fetch is working

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,51 @@ async function getData() {
   return res.json()
 }
 
+// put this api fetch in a seperate file
+// add pagination
+async function getAnime(query: string) {
+  let variables = {
+    search: query,
+    page: 1,
+    perPage: 10,
+  };
 
+  const res = await fetch('https://graphql.anilist.co', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `query ($page: Int, $perPage: Int, $search: String) {
+        Page(page: $page, perPage: $perPage) {
+          pageInfo {
+            total
+            perPage
+          }
+          media(search: $search, type: ANIME, sort: FAVOURITES_DESC) {
+            id
+            title {
+              romaji
+              english
+              native
+            }
+            type
+            genres
+          }
+        }
+      }
+      `, 
+      variables
+    }),
+    next: { revalidate: 10 },
+  })
+
+  return res.json()
+}
 
 
 export default async function Home() {
-  const data = await getData()
+  const data = await getAnime("Ichigo")
  
   return (
     <h2>{JSON.stringify(data)}</h2>
