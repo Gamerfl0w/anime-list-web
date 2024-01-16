f033fce9fa7dc3550e63746176f6a7f77b1ec6c0
gamerfl0w <jluisdechavez@gmail.com>
2024-01-16 19:33:37 +0800

    deploy fix

--- pages/layouts/navbar.tsx
+++ pages/layouts/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-export default function navbar(){
+export default function Navbar(){
     const { data: session, status } = useSession()
 
     return (
