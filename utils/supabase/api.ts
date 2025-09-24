23f83fdb200cc9c1ef2b5be0c2bebb5332bb3a50
Gamerfl0w <jluisdechavez@gmail.com>
2025-09-24 13:28:44 +0800

    update environment variables

--- utils/supabase/api.ts
+++ utils/supabase/api.ts
@@ -3,8 +3,8 @@ import { type NextApiRequest, type NextApiResponse } from 'next'
 
 export default function createClient(req: NextApiRequest, res: NextApiResponse) {
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll() {
